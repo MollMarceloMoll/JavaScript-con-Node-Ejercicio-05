9d6252e57c463a16a576d8e11cc4e0d9f06ac00b
Marcelo Moll <75146662+MollMarceloMoll@users.noreply.github.com>
2023-01-24 19:28:25 -0300

    Ejercicio 05
    Enunciado del ejercicio:
    
    Crea un archivo JS que contenga las siguientes líneas
    
    - Una variable que contenga tu altura en centímetros (entero)
    
    - Una variable que contenga tu altura en metros (número de coma flotante)
    
    - Una variable que contenga tu peso en kilogramos (número de coma flotante)
    
    - Una variable que contenga tu altura en metros redondeada hacia arriba
    
    - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
    
    - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript

---
+++ numeros.js
@@ -0,0 +1,12 @@
+let  alturaCm = 190
+let alturaM = 1.90
+let pesoKg = 90.4
+
+let alturaRedondeada = Math.ceil(alturaCm);
+console.log(alturaRedondeada);
+
+let pesoRedondeado = Math.floor(pesoKg);
+console.log(pesoRedondeado);
+
+let valoresIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+console.log(valoresIguales)
